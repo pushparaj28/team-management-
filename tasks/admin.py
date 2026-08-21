81bf5f4dae6929881deadd73f03d38c71b4abc86
Khushboo Kushwaha <khushbookushwah9259@gmail.com>
2026-08-21 14:42:22 +0530

    feat: add Milestone and Task models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Milestone, Task
 
+
 @admin.register(Milestone)
 class MilestoneAdmin(admin.ModelAdmin):
     list_display = ('title', 'target_date', 'status')
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
