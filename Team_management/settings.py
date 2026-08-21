ae3b8c6a10467c234126ce677acbabf5cf19350e
Khushboo Kushwaha <khushbookushwah9259@gmail.com>
2026-08-21 13:06:39 +0530

    chore: register tasks app and wire up urls

--- Team_management/settings.py
+++ Team_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'tasks',
 
 ]
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Email
