4004f293d3975f00a33a1b85a42371c9e071cb0f
Pushpendra Singh <hacktag1949@gmail.com>
2026-08-28 10:15:24 +0530

    Merge friend branch into singh and resolve conflicts

--- Team_management/urls.py
+++ Team_management/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('tasks/', include('tasks.urls')),
+    path('resources/', include('resource.urls')),
     path('', RedirectView.as_view(pattern_name='accounts:login'), name='home'),
 ]
 
